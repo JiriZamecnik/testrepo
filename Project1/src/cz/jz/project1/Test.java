bd6e3ae229867e0f9dd47ca0535ec4d7decb6dc0
Jiří Zámečník <jzamecnik@csob.cz>
2017-08-28 15:45:41 +0200

    Úprava 1

--- Project1/src/cz/jz/project1/Test.java
+++ Project1/src/cz/jz/project1/Test.java
@@ -8,14 +8,17 @@ public class Test {
 
 	public static void main(String[] args) {
 		System.out.println("Start.");
-		List<Integer> list = Arrays.asList(1, 2, 3, 5, 4, 8, 6, 7, -1, 2);
+		List<Integer> list = Arrays.asList(1, 2, 3, 5, 4, 8, 6, 7, -1, 2, 16, 11, 13);
+		
 		System.out.println(list);
+		
 		list.sort(new Comparator<Integer>() {
 			public int compare(Integer i1, Integer i2) {
 				return i1.compareTo(i2);
 			}
 		});
 		System.out.println(list);
+		
 		System.out.println("Stop.");
 	}
 
