3d9e961075a5bbb9f3296f2686b64b7c94a20111
Jiří Zámečník <jirkovi@gmail.com>
2017-12-16 19:28:18 +0100

    Test

--- Project1/src/cz/jz/project1/Test.java
+++ Project1/src/cz/jz/project1/Test.java
@@ -8,7 +8,7 @@ public class Test {
 
 	public static void main(String[] args) {
 		System.out.println("Start.");
-		List<Integer> list = Arrays.asList(1, 2, 3, 5, 4, 8, 6, 7, -1, 2, 16, 11, 13, -15);
+		List<Integer> list = Arrays.asList(1, 2, 3, 5, 4, 8, 6, 7, -1, 2, 16, 11, 13, -15, 45);
 
 		System.out.println(list);
 
